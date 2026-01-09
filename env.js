ec8f1f78574ae1601d61e55e11b8b8e8546b0400
Alejandro Rode <alejandro.rode@gmail.com>
2026-01-09 13:32:49 -0300

    chore(prod): actualizar env.js a Supabase PROD-NUEVA

--- env.js
+++ env.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 window.__ENV__ = {
-  SUPABASE_URL: "https://sxfnrkpkgnvvakqvfhzr.supabase.co",
+  SUPABASE_URL: "https://vuzayowerushcxzjumvf.supabase.co",
   SUPABASE_ANON_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4Zm5ya3BrZ252dmFrcXZmaHpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MDIyNjYsImV4cCI6MjA3NjI3ODI2Nn0.NyxV95OviEYlSnvVKbvXATsqMXyO5eDp6McPN-a6XwU"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1emF5b3dlcnVzaGN4emp1bXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4ODYxMjcsImV4cCI6MjA4MzQ2MjEyN30.naZH3znh2cqFNy7pKT-mmstZWMHb9Fg2ZrwOyxqfDvc"
 };
